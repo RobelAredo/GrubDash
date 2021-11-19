8394fc98740f17d4edbcb236e52d04d658045b0c
Robel Aredo <ichabodvancha@yahoo.com>
2021-11-19 13:46:09 -0600

    Orders controller and router finished except for the delete method

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -11,29 +11,39 @@ const list = (req, res) => {
   res.json({ data : orders });
 };
 
+const validateStatus = (acc, status, order) => {
+  if (!order) return;
+  const validStatuses = [ "pending", "preparing", "out-for-delivery"];
+  if (status === "delivered") acc.push("A delivered order cannot be changed");
+  else if (!validStatuses.some(validStatus => status === validStatus)) {
+    acc.push("Order must have a status of pending, preparing, out-for-delivery, delivered");
+  };
+}
+
 const validiteDishes = (acc, dishes) => {
   if (!Array.isArray(dishes) || !dishes.length) {
     acc.push("Order must include at least one dish")
     return;
   }
   const errors = dishes.forEach(({quantity}, i) => {
-    if (!++quantity || quantity < 0) acc.push(`Dish ${i} must have a quantity that is an integer greater than 0`)
+    if (!Number.isInteger(quantity) || quantity <= 0) acc.push(`Dish ${i} must have a quantity that is an integer greater than 0`)
   })
 }
 
 const hasAllProperties = (req, res, next) => {
-  const { params: { orderId }, body: { data: { id } } } = req
-  if(id && !(id == orderId)) return next({
+  const { params: { orderId }, body: { data: { id } } } = req;
+  if(orderId && id && !(id == orderId)) return next({
       status: 400,
-      message: `Dish id does not match route id. Dish: ${id}, Route: ${dishId}`,
-    })
+      message: `Order id does not match route id. Order: ${id}, Route: ${orderId}`,
+  })
 
   const props = ["deliverTo", "mobileNumber", "status", "dishes"];
   res.locals.newOrder = {};
   errors = props.reduce((acc, prop) => {
     const propValue = req.body.data[prop];
     expected = `Order must include `
-    if(!propValue) acc.push(expected + `a ${prop}`)
+    if(!propValue && prop !== "status") acc.push(expected + `a ${prop}`)
+    else if (prop === "status") validateStatus(acc, propValue, res.locals.order)
     else if (prop === "dishes") validiteDishes(acc, propValue)
     else res.locals.newOrder[prop] = propValue;
     return acc;
@@ -73,9 +83,15 @@ const read = (req, res) => {
   res.json({ data: order });
 }
 
-const destroy = (req, res) => {
+const update = (req, res) => {
+  const { order, newOrder } = res.locals;
+  Object.assign(order, newOrder);
+  res.json({ data: order });
+}
+
+const destroy = (req, res, next) => {
   const { order } = res.locals;
-  if (order.status) return next({status: 404, message: "An order cannot be deleted unless it is pending"});
+  if (order.status !== "pending") return next({status: 400, message: "An order cannot be deleted unless it is pending"});
 
   const { orderId } = req.params;
   const index = orders.indexOf(order => order.id === orderId);
--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,16 +1,16 @@
 const router = require("express").Router();
-const { list, create, read, update, delete } = require("./orders.controller");
+const controller = require("./orders.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
 router.route("/:orderId")
-  .get(read)
-  .post(update)
-  .delete(delete)
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
   .all(methodNotAllowed);
   
 router.route("/")
-  .get(list)
-  .post(create)
+  .get(controller.list)
+  .post(controller.create)
   .all(methodNotAllowed);
   
 module.exports = router;
